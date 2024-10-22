5d6fdad18312dc049ed30d50d853a8d121afd524
Scott Luu <scott@boostsecurity.io>
2024-10-22 12:32:49 -0400

    balancing

--- frontend/src/hooks/useSpecializationPoints.ts
+++ frontend/src/hooks/useSpecializationPoints.ts
@@ -4,10 +4,24 @@ import useAppSelector from "./useAppSelector";
 const useSpecializationPoints = () => {
   const moneyPerSecond = useMoneyPerSecond();
   const money = useAppSelector((state) => state.money.value);
+  const bugFixers = useAppSelector((state) => state.bugFixers.value);
+  const featureDevelopers = useAppSelector(
+    (state) => state.featureDevelopers.value,
+  );
+  const soldCompanies = useAppSelector((state) => state.soldCompanies.value);
 
-  const moneyPerSecondComponent = Math.floor(Math.pow(moneyPerSecond, 0.5));
+  const moneyPerSecondComponent = Math.pow(moneyPerSecond, 0.6);
   if (moneyPerSecondComponent < 1) return 0;
-  return moneyPerSecondComponent + Math.floor(Math.pow(money, 1 / 10));
+  return Math.max(
+    0,
+    Math.floor(
+      moneyPerSecondComponent / 2 +
+        Math.pow(money, 1 / 5) / 2 +
+        Math.pow(bugFixers + featureDevelopers, 0.5) -
+        2 -
+        Math.pow(1.25, soldCompanies.length),
+    ),
+  );
 };
 
 export default useSpecializationPoints;
